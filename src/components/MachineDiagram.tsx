c7bf3fc4627ea0e4158cd60da781e6f6797801bc
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-10-10 21:10:41 +0800

    feat: proper tokenization

--- src/components/MachineDiagram.tsx
+++ src/components/MachineDiagram.tsx
@@ -1,6 +1,14 @@
+import { createEffect, createSignal } from "solid-js"
+import getMachine from "~/utils/getMachine"
+
 export default function MachineDiagram(props : {
     machineSpec : string
-}) {
+}){
+    createEffect(() => {
+        console.log(props.machineSpec)
+        console.log(getMachine(props.machineSpec))
+    }, [props.machineSpec])
+
     return (
         <>
             <h2> Machine </h2>
