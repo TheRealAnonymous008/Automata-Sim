244cc824d68d7e86fdbd555f191aac9f88ceeb4a
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-10-11 23:42:39 +0800

    feat: arrowheads for transitions

--- src/components/Coordinate.tsx
+++ src/components/Coordinate.tsx
@@ -2,3 +2,9 @@ export default interface Coordinate {
     x : number, 
     y : number
 }
+
+export function getDistance(p1 : Coordinate, p2 : Coordinate){
+    let dx = p1.x - p2.x 
+    let dy = p1.y - p2.y
+    return Math.sqrt(dx * dx + dy * dy)
+}
--- src/components/TransitionComponent.tsx
+++ src/components/TransitionComponent.tsx
@@ -1,22 +1,39 @@
 import State, { getAllTransitions } from "~/models/states/state";
-import Coordinate from "./Coordinate";
+import Coordinate, { getDistance } from "./Coordinate";
 import { Machine } from "~/models/machine";
 import { getValuesInMap } from "~/utils/dictToList";
+import { STATE_CIRCRADIUS } from "./StateComponent";
 
 export default function TransitionComponent(props: {
     src : Coordinate,
     dest : Coordinate
 }){
+    const offx =  props.dest.x - props.src.x
+    const offy = props.dest.y - props.src.y
     return (
-        <line 
-            x1={props.src.x} 
-            y1={props.src.y} 
-            x2={props.dest.x} 
-            y2={props.dest.y} 
-            stroke="black" 
-            stroke-width="3" 
-            marker-mid="url(#arrowhead)"
-        />
+        <>
+            <defs>
+                <marker 
+                    id="arrowhead" 
+                    markerWidth="10" 
+                    markerHeight="7" 
+                    refX={5 + STATE_CIRCRADIUS / 2.5}
+                    refY="3.5"
+                    orient="auto">
+                    <polygon points="0 0, 10 3.5, 0 7" />
+                </marker>
+            </defs>
+
+            <line 
+                x1={props.src.x} 
+                y1={props.src.y} 
+                x2={props.dest.x} 
+                y2={props.dest.y} 
+                stroke="black" 
+                stroke-width="3" 
+                marker-end="url(#arrowhead)"
+            />
+        </>
     )
 }
 
